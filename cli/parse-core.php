db35460de65ba684abafe17712796f84b9e59cc1
David Mudrak <david.mudrak@gmail.com>
2010-04-01 16:19:33 +0200

    Search for string files in all locations
    Having a list of locations did not work when a file got moved from one
    location to another.

--- cli/parse-core.php
+++ cli/parse-core.php
@@ -56,28 +56,6 @@ while(@ob_end_flush());
 // increase memory limit (PHP 5.2 does different calculation, we need more memory now)
 @raise_memory_limit('128M');
 
-// prepare the list of valid locations of string files
-$locations = array('lang/en_utf8/*.php');
-$plugins = string_manager::instance()->get_registered_plugin_types();
-foreach ($plugins as $prefixtype => $pluginlocations) {
-    if (empty($prefixtype)) {
-        $plugintype = '';
-    } else {
-        $plugintype = substr($prefixtype, 0, strlen($prefixtype)-1);
-    }
-    foreach ($pluginlocations as $pluginlocation) {
-        $pluginlist = get_plugin_list($plugintype);
-        foreach ($pluginlist as $plugintype => $pluginpath) {
-            $langfile = substr($pluginpath . '/lang/en_utf8/' . $prefixtype . $plugintype  . '.php', strlen($CFG->dirroot) + 1);
-            if (file_exists(AMOS_REPO_MOODLE . '/' . $langfile)) {
-                $locations[] = $langfile;
-            }
-        }
-    }
-}
-$locations = implode(' ', $locations);
-echo "SEARCH $locations\n";
-
 $tmp = make_upload_directory('amos/temp', false);
 $var = make_upload_directory('amos/var', false);
 $mem = memory_get_usage();
@@ -98,6 +76,48 @@ $MLANG_BROKEN_CHECKOUTS = array(
     'c5d0eaa9afecd924d720fbc0b206d144eb68db68_lang_en_utf8_question.php',
     '06e84d52bd52a4901e2512ea92d87b6192edeffa_lang_en_utf8_error.php',
     '4416da02db714807a71d8a28c19af3a834d2a266_lang_en_utf8_enrol_mnet.php',
+);
+
+$MLANG_IGNORE_COMMITS = array(
+    //
+    // the following are MDL-21694 commits that just move the lang files. such a move is registered
+    // as a deletion and re-addition of every string which is usually useless
+    //
+    '9d68aee7860398345b3921b552ccaefe094d438a',
+    '5f251510549671a3864427e4ea161b8bd62d0df9',
+    '60b00b6d99f10c084375d09c244f0011baabdec9',
+    'f312fe1a9e00abe1f79348d1092697a485369bfb',
+    '05162f405802faf006cac816443432d29e742458',
+    '57223fbe95df69ebb9831ff681b89ec67de850ff',
+    '7ae8954a02ebaf82f74e2842e4ad17c05f6af6a8',
+    '1df58edc0f25db3892950816f6b9edb2de693a2c',
+    'd8753184ec66575cffc834aaeb8ac25477da289b',
+    '200fe7f26b1ba13d9ac63f073b6676ce4abd2976',
+    '2476f5f22c2bfaf0626a7e1e8af0ffee316b01b4',
+    'd8a81830333d99770a6072ddc0530c267ebddcde',
+    'afbbc6c0f6667a5af2a55aab1319f3be00a667f1',
+    '3158eb9350ed79c3fe81919ea8af67418de18277',
+    'ffbb41f48f9c317347be4288771db84e36bfdf22',
+    '81144b026f80665a7d7ccdadbde4e8f99d91e806',
+    '675aa51db3038b629c7350a53e43f20c5d414045',
+    'dee576ebbaece98483acfa401d459f62f0f0387d',
+    'eea1d899bca628f9c5e0234068beb713e81a64fd',
+    'ce0250ec19cf29479b36e17541c314030a2f9ab5',
+    'bda5f9b1159bff09006dac0bcfaec1ec788f134c',
+    '89422374d1944d4f5fff08e2187f2c0db75aaefc',
+    'b4340cb296ce7665b6d8f64885aab259309271a6',
+    '001fa4b3135b27c2364845a221d11ea725d446a0',
+    'c811222ff9b1469633f7e8dbf6b06ddccafb8dbd',
+    '7a4ddc172ae46014ee2ebb5b9f4ee2ada2cd7e1e',
+    'bc3755be21025c5815de19670eb04b0875f5fa31',
+    '96b838efa990d6a6a2db0050d9deeceeda234494',
+    'cb9dc45c36ffbbdee1a0f22a50b4f31db47a5eb6',
+    '33aadb2d70c4e8381281b635a9012f3f0673d397',
+    '34970b7fc6c4932b15426ea80ad94867a1e1bb5b',
+    '7a563f0f3586a4bc5b5263492282734410e01ee0',
+    'b13af519fc48ee9d8b1e801c6056519454bf8400',
+    'd1f62223b59d6acb1475d3979cdafda726cc1290',
+    '2064cbaa0f6ea36fc5803fcebb5954ef8c642ac4',
 );
 
 $MLANG_PARSE_BRANCHES = array(
@@ -130,7 +150,7 @@ foreach ($MLANG_PARSE_BRANCHES as $branch) {
     chdir(AMOS_REPO_MOODLE);
     $gitout = array();
     $gitstatus = 0;
-    $gitcmd = "git whatchanged --reverse --format=format:COMMIT:%H {$gitbranch} {$startat} {$locations}";
+    $gitcmd = "git whatchanged --reverse --format=format:COMMIT:%H {$gitbranch} {$startat}";
     echo "RUN {$gitcmd}\n";
     exec($gitcmd, $gitout, $gitstatus);
 
@@ -146,14 +166,26 @@ foreach ($MLANG_PARSE_BRANCHES as $branch) {
             continue;
         }
         if (substr($line, 0, 7) == 'COMMIT:') {
-            file_put_contents($startatlock, $commithash);
+            // remember the processed commithash
+            if (!empty($commithash)) {
+                file_put_contents($startatlock, $commithash);
+            }
             $commithash = substr($line, 7);
+            continue;
+        }
+        if (in_array($commithash, $MLANG_IGNORE_COMMITS)) {
+            echo "IGNORED {$commithash}\n";
             continue;
         }
         $parts = explode("\t", $line);
         $changetype = substr($parts[0], -1);    // A (added new file), M (modified), D (deleted)
         $file = $parts[1];
+        if (!strstr($file, 'lang/en_utf8/')) {
+            // this is not a language file
+            continue;
+        }
         if (substr($file, -4) !== '.php') {
+            // this is not a valid string file
             continue;
         }
         $memprev = $mem;
