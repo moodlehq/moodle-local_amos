42d533b45fb5e831b9434b32a92113f79c82bca2
David Mudrak <david.mudrak@gmail.com>
2010-04-22 23:28:36 +0200

    Use the index so we can filter English strings only quickly

--- version.php
+++ version.php
@@ -25,6 +25,6 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version  = 2010030200;
+$plugin->version  = 2010042200;
 $plugin->requires = 2010012500;  // Requires this Moodle version
 //$module->cron     = 60;
