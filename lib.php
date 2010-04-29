22406160ca6249e2de9834fb4a933320f3fc5774
David Mudrak <david.mudrak@gmail.com>
2010-04-29 22:07:53 +0200

    Fixing a link in the nav tree

--- lib.php
+++ lib.php
@@ -33,6 +33,6 @@ defined('MOODLE_INTERNAL') || die();
 function amos_extends_navigation(global_navigation $navigation) {
     $amos = $navigation->add('AMOS', new moodle_url('/local/amos/'));
     $amos->add('Translator', new moodle_url('/local/amos/view.php'));
-    $amos->add('Stage', new moodle_url('/local/amos/view.php'));
+    $amos->add('Stage', new moodle_url('/local/amos/stage.php'));
     $amos->add('Log', new moodle_url('/local/amos/log.php'));
 }
