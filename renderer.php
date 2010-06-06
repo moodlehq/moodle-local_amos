731c4cc0774d627dd5d2968e1a6a1ea811d82279
David Mudrak <david.mudrak@gmail.com>
2010-06-06 14:56:42 +0200

    MDL-22675 Fixing XHTML issue

--- renderer.php
+++ renderer.php
@@ -70,7 +70,7 @@ class local_amos_renderer extends plugin_renderer_base {
         }
         unset($options['en']); // English is not translatable via AMOS
         $output .= html_writer::select($options, 'flng[]', $filter->get_data()->language, '',
-                    array('id' => 'amosfilter_flng', 'multiple' => true, 'size' => 2));
+                    array('id' => 'amosfilter_flng', 'multiple' => 'multiple', 'size' => 2));
         $output .= html_writer::tag('span', '', array('id' => 'amosfilter_flng_actions', 'class' => 'actions'));
         $output .= html_writer::end_tag('div');
         $output .= html_writer::end_tag('div');
@@ -87,7 +87,7 @@ class local_amos_renderer extends plugin_renderer_base {
             $options[$componentname] = $componentname;
         }
         $output .= html_writer::select($options, 'fcmp[]', $filter->get_data()->component, '',
-                    array('id' => 'amosfilter_fcmp', 'multiple' => true, 'size' => 5));
+                    array('id' => 'amosfilter_fcmp', 'multiple' => 'multiple', 'size' => 5));
         $output .= html_writer::tag('span', '', array('id' => 'amosfilter_fcmp_actions', 'class' => 'actions'));
         $output .= html_writer::end_tag('div');
         $output .= html_writer::end_tag('div');
