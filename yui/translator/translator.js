15f1f7256447aaa0eba688362656722743fc0b64
David Mudrák <david@moodle.com>
2014-07-10 16:25:56 +0200

    Split the translator JS module into filter and the translator itself

--- yui/translator/translator.js
+++ yui/translator/translator.js
@@ -23,121 +23,16 @@ YUI.add('moodle-local_amos-translator', function(Y) {
          */
         ajaxqueue: [],
 
-        /**
-         * The text currently entered into the search component box
-         *
-         * @property filtersearchneedle
-         */
-        filtersearchneedle: '',
-
         initializer : function(config) {
             this.init_translator();
+            Y.log('Translator initialized', 'debug', 'moodle-local_amos-translator');
         },
 
         /**
          * Initialize JS support for the main translation page
          */
         init_translator: function() {
-            var filter      = Y.one('#amosfilter');
             var translator  = Y.one('#amostranslator');
-
-            // add select All / None links to the filter languages selector field
-            var flng        = filter.one('#amosfilter_flng');
-            var flngactions = filter.one('#amosfilter_flng_actions');
-            var flnghtml    = '<a href="#" id="amosfilter_flng_actions_all">' +
-                              M.util.get_string('languagesall', 'local_amos') + '</a>' +
-                              ' / <a href="#" id="amosfilter_flng_actions_none">' +
-                              M.util.get_string('languagesnone', 'local_amos') + '</a>';
-            flngactions.set('innerHTML', flnghtml);
-            var flngselectall = filter.one('#amosfilter_flng_actions_all');
-            flngselectall.on('click', function(e) { e.preventDefault(); flng.all('option').set('selected', true); });
-            var flngselectnone = filter.one('#amosfilter_flng_actions_none');
-            flngselectnone.on('click', function(e) { e.preventDefault(); flng.all('option').set('selected', false); });
-
-            // add select All / None links to the filter components selector field
-            var fcmp        = filter.one('#amosfilter_fcmp');
-            var fcmpactions = filter.one('#amosfilter_fcmp_actions');
-            var fcmphtml    = '<a href="#" id="amosfilter_fcmp_actions_enlarge">' +
-                              M.util.get_string('componentsenlarge', 'local_amos') + '</a>' +
-                              ' / <a href="#" id="amosfilter_fcmp_actions_allstandard">' +
-                              M.util.get_string('componentsstandard', 'local_amos') + '</a>' +
-                              ' / <a href="#" id="amosfilter_fcmp_actions_all">' +
-                              M.util.get_string('componentsall', 'local_amos') + '</a>' +
-                              ' / <a href="#" id="amosfilter_fcmp_actions_none">' +
-                              M.util.get_string('componentsnone', 'local_amos') + '</a>' +
-                              ' <input type="text" size="8" maxlength="20" placeholder="' + M.util.get_string('search', 'core') +
-                                '" name="amosfilter_fcmp_actions_search" class="search-query" id="amosfilter_fcmp_actions_search" />';
-            fcmpactions.set('innerHTML', fcmphtml);
-            var fcmpenlarge = filter.one('#amosfilter_fcmp_actions_enlarge');
-            fcmpenlarge.on('click', function(e) {
-                // Enlarge the components selection box.
-                e.preventDefault();
-                fcmp.setStyle('height', parseInt(fcmp.getComputedStyle('height')) * 2 + 'px');
-            });
-            var fcmpselectallstandard = filter.one('#amosfilter_fcmp_actions_allstandard');
-            fcmpselectallstandard.on('click', function(e) {
-                // Check all displayed standard components.
-                e.preventDefault();
-                fcmp.all('tr.standard:not(.hidden) input').set('checked', true);
-            });
-            var fcmpselectall = filter.one('#amosfilter_fcmp_actions_all');
-            fcmpselectall.on('click', function(e) {
-                // Check all displayed components.
-                e.preventDefault();
-                fcmp.all('tr:not(.hidden) input').set('checked', true);
-            });
-            var fcmpselectnone = filter.one('#amosfilter_fcmp_actions_none');
-            fcmpselectnone.on('click', function(e) {
-                // Uncheck all components (even those not displayed).
-                e.preventDefault();
-                fcmp.all('tr input').set('checked', false);
-            });
-
-            // search for components
-            var fcmpsearch = filter.one('#amosfilter_fcmp_actions_search');
-            fcmpsearch.on('keypress', function(e) {
-                if (e.keyCode == 13) {
-                    // Do not submit the form
-                    e.halt();
-                }
-            });
-            fcmpsearch.on('valuechange', this.filter_components, this, fcmpsearch, fcmp);
-
-            // scroll the components selector to the first checked item
-            var firstcheckcomponent = fcmp.one('.labelled_checkbox.preset');
-            if (firstcheckcomponent) {
-                // Align with bottom to prevent the main window from scrolling too.
-                firstcheckcomponent.scrollIntoView(false);
-            }
-
-            // make greylist related checkboxed mutally exclusive
-            var fglo = filter.one('#amosfilter_fglo');
-            var fwog = filter.one('#amosfilter_fwog');
-            fglo.on('change', function(e) {
-                if (fglo.get('checked')) {
-                    fwog.set('checked', false);
-                }
-            });
-            fwog.on('change', function(e) {
-                if (fwog.get('checked')) {
-                    fglo.set('checked', false);
-                }
-            });
-
-            // display the "loading" icon after the filter button is pressed
-            var fform       = Y.one('#amosfilter_form');
-            var fsubmit     = fform.one('button[type="submit"]');
-            var translatorw = Y.one('.translatorwrapper');
-            var ficonholder = fform.one('#amosfilter_submitted_icon');
-            var ficonhtml   = '<img src="'+M.cfg.loadingicon+'" class="spinner" style="display:none" />';
-            ficonholder.set('innerHTML', ficonhtml);
-
-            fform.on('submit', function(e) {
-                        fsubmit.setStyle('minWidth', fsubmit.getComputedStyle('width'));
-                        fsubmit.set('text', M.util.get_string('processing', 'local_amos'));
-                        ficonholder.one('img').setStyle('display', 'inline');
-                        translatorw.setStyle('display', 'none');
-                        });
 
             if (translator) {
 
@@ -177,29 +72,6 @@ YUI.add('moodle-local_amos-translator', function(Y) {
                 // initialize the google translate service support
                 this.init_google_translator();
             }
-        },
-
-        /**
-         * Filter the list of components
-         *
-         * @method filter_components
-         * @param {Y.Event} e
-         * @param {Y.Node} searchfield
-         * @param {Y.Node} componentslist the wrapping node of the list of components
-         */
-        filter_components: function(e, searchfield, componentslist) {
-            this.filtersearchneedle = searchfield.get('value').toString().replace(/^ +| +$/, '');
-            componentslist.all('table tr td.labelled_checkbox').each(function(component, index, componentslist) {
-                if (this.filtersearchneedle == '' || component.one('label').get('text').toString().indexOf(this.filtersearchneedle) !== -1) {
-                    row = component.get('parentNode');
-                    row.show();
-                    row.removeClass('hidden');
-                } else {
-                    row = component.get('parentNode');
-                    row.hide();
-                    row.addClass('hidden');
-                }
-            }, this);
         },
 
         /**
