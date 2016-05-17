4d8038d0160154f99461911cf04a1f3979975806
David Mudrák <david@moodle.com>
2016-05-17 10:54:04 +0200

    Consistently quote string identifiers in logging output

--- cli/utilslib.php
+++ cli/utilslib.php
@@ -800,7 +800,7 @@ class amos_merge_string_files {
 
         foreach ($tostrings as $changeid => $changetext) {
             if (!isset($fromstrings[$changeid])) {
-                $this->log('Attempting to merge an orphaned change: '.$changeid, amos_cli_logger::LEVEL_DEBUG);
+                $this->log('Attempting to merge an orphaned change: "'.$changeid.'"', amos_cli_logger::LEVEL_DEBUG);
                 continue;
             }
             if ($fromstrings[$changeid] !== $changetext) {
@@ -822,7 +822,7 @@ class amos_merge_string_files {
                 }
                 $realchanges += $count;
             } else {
-                $this->log('String '.$changeid.' identical, no update needed', amos_cli_logger::LEVEL_DEBUG);
+                $this->log('String "'.$changeid.'" identical, no update needed', amos_cli_logger::LEVEL_DEBUG);
             }
         }
 
