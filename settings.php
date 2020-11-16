69cb35b3758e6c9400806af859af9da92f28aa31
David Mudrák <david@moodle.com>
2020-11-16 10:51:44 +0100

    Put 311 among default settings

--- settings.php
+++ settings.php
@@ -33,7 +33,7 @@ if ($hassiteconfig) {
             'local_amos/branchesall',
             get_string('branchesall', 'local_amos'),
             get_string('branchesall_desc', 'local_amos'),
-            '20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,310,400',
+            '20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,310,311,400',
             PARAM_RAW_TRIMMED,
             60, 3
         ));
